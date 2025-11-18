f18964437af652246b5b39e69028b077840b0a44
will0402 <70189184+will0402@users.noreply.github.com>
2025-11-18 18:18:58 +0800

    Reorganize imports and update API request flow

--- weather_dashboard.py
+++ weather_dashboard.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import requests
 
-response = requests.get(url, verify=False)
+
 
 st.title("台灣氣象資料 Dashboard")
 
@@ -11,6 +11,8 @@ API_KEY="CWA-1B9E1D40-6BE6-42BC-9B4E-C523C3BBB1F1"
 LOCATION=st.selectbox("選擇城市",["Taipei","Taichung","Kaohsiung"])#
 
 url = f"https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization={API_KEY}&format=JSON&locationName=%E8%87%BA%E5%8C%97%E5%B8%82"
+
+response = requests.get(url, verify=False)
 
 res = requests.get(url)
 data = res.json()
@@ -23,3 +25,4 @@ for element in location ["weatherElement"]:
     value = element ["time"] [0] ["parameter"] ["parameterName"]
 
     st.write(f"{name} : {value}")
+
